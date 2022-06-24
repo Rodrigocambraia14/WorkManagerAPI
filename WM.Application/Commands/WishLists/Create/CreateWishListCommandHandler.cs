775170f579fbcb2c356126821539046e9154c990
Rodrigo Gonçalves Cambraia Soares <rodrigocambraia.jr@gmail.com>
2022-06-24 16:15:00 -0300

    create withList API

--- WM.Application/Commands/WishLists/Create/CreateWishListCommandHandler.cs
+++ WM.Application/Commands/WishLists/Create/CreateWishListCommandHandler.cs
@@ -1,10 +1,12 @@
 ﻿using AutoMapper;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WM.Context.Default.Entities;
 using WM.CrossCutting.Enums;
 using WM.CrossCutting.Helper;
 using WM.Infra.Context.Persistence.Context.Default;
@@ -27,7 +29,28 @@ namespace WM.Application.Commands.WishLists.Create
             if (this.defaultContext.WishListUsers.Where(x => x.UserId == command.UserId && x.UserType == WishListUserType.Owner).Count() >= 20)
                 throw new Exception("Você alcançou o limite de listas permitidas !");
 
-            return ContractResponse.ValidContractResponse(string.Empty);
+            if(await this.defaultContext.WishLists.AnyAsync(x => x.Name == command.Name && x.WishListUsers.Any(y => y.UserId == command.UserId && y.UserType == WishListUserType.Owner), cancellationToken: cancellationToken))
+                throw new Exception("Você já possui uma lista com este nome !");
+
+            var wishList = this.mapper.Map<WishList>(command,
+                    opt => opt.AfterMap((src, dest) =>
+                    {
+                        dest.CreatedDate = DateTime.Now;
+                        //dest.Products = this.mapper.Map<List<Product>>(command.Products);
+                    }));
+
+            await this.defaultContext.WishLists.AddAsync(wishList, cancellationToken);
+
+            await this.defaultContext.WishListUsers.AddAsync(new WishListUser()
+            {
+                UserId = command.UserId,
+                UserType = WishListUserType.Owner,
+                WishListId = wishList.Id
+            }, cancellationToken);
+
+            await this.defaultContext.SaveChangesAsync(cancellationToken);
+
+            return ContractResponse.ValidContractResponse("Lista criada com sucesso !");
         }
     }
 }
---
+++ WorkManagerAPI/Controllers/WishListController.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using WM.Application.Commands.WishLists.Create;
+
+namespace WorkManagerAPI.Controllers
+{
+    [Route("api/wishList")]
+    [Produces("application/json")]
+    [ApiController]
+    public class WishListController : ControllerBase
+    {
+        private readonly IMediator mediator;
+
+        public WishListController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        [HttpPost("create")]
+        public async Task<IActionResult> Create([FromBody] CreateWishListCommand command)
+        {
+            return Created(string.Empty, await this.mediator.Send(command));
+        }
+    }
+}
